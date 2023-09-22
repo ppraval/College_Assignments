0bbf66eaf4985b6e40e270d8d0af342f17684d21
PravalPattam <ppraval@live.com>
2023-09-22 18:06:44 +0530

    New assignment

---
+++ S3/PD_lab_Assignments/ASSG5A_B220057CS_PRAVAL/ASSG5A_B220057CS_PRAVAL_1.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int merge(int arr[], int p, int q, int r)
+{
+	int i, j, k;
+	int n1 = q - p + 1;
+	int n2 = r - q;
+	int L[n1], R[n2];
+
+	for (i = 0; i < n1; i++)
+    {
+		L[i] = arr[p + i];
+    }
+    for (j = 0; j < n2; j++)
+    {
+		R[j] = arr[q + 1 + j];
+    }
+	i = 0;
+	j = 0;
+	k = p;
+	while (i < n1 && j < n2) 
+    {
+		if (L[i] <= R[j]) 
+        {
+			arr[k] = L[i];
+			i++;
+		}
+		else 
+        {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	while (i < n1) 
+    {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	while (j < n2) 
+    {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+    return 0;
+}
+
+int mergeSort(int arr[], int p, int r)
+{
+	if (p < r) 
+    {
+		int q = p + (r - p) / 2;
+
+		mergeSort(arr, p, q);
+		mergeSort(arr, q + 1, r);
+
+		merge(arr, p, q, r);
+	}
+    return 0;
+}
+
+int main()
+{
+	int n;
+    scanf("%d", &n);
+    int arr[100000];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+	mergeSort(arr, 0, n - 1);
+    
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            printf("%d", arr[i]);
+        }
+        else
+        {
+            printf(" %d", arr[i]);
+        }
+    }
+    return 1;
+}
---
+++ S3/PD_lab_Assignments/ASSG5A_B220057CS_PRAVAL/ASSG5A_B220057CS_PRAVAL_2.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+int swap(int a, int b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+    return 0;
+}
+
+int heapify(int arr[], int n, int i)
+{
+	int largest = i;
+	int left = 2 * i + 1;
+	int right = 2 * i + 2;
+    int temp;
+
+	if (left < n && arr[left] > arr[largest])
+    {
+		largest = left;
+    }
+    if (right < n && arr[right] > arr[largest])
+	{
+    	largest = right;
+    }
+    if (largest != i) 
+    {
+        temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+		heapify(arr, n, largest);
+	}
+    return 0;
+}
+
+int heapSort(int arr[], int n)
+{
+    int temp;
+	for (int i = n / 2 - 1; i >= 0; i--)
+	{
+    	heapify(arr, n, i);
+    }
+
+	for (int i = n - 1; i >= 0; i--) 
+    {
+		temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+		heapify(arr, i, 0);
+	}
+
+    return 0;
+}
+
+
+int main()
+{
+	int n;
+    scanf("%d", &n);
+    int arr[100000];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    heapSort(arr, n);
+    
+    for (int i = 0; i < n; i++)
+    {
+        if (i == 0)
+        {
+            printf("%d", arr[i]);
+        }
+        else
+        {
+            printf(" %d", arr[i]);
+        }
+    }
+    return 1;
+}
