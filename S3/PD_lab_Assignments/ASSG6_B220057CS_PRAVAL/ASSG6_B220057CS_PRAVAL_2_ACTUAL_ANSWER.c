a69bb83c33dac7a7937cebf5794be53626300668
PravalPattam <ppraval@live.com>
2023-10-09 00:00:27 +0530

    Renamed files

--- S3/PD_lab_Assignments/ASSG6_B220057CS_PRAVAL/ASSG6_B220057CS_PRAVAL_2_ACTUAL_ANSWER.c
+++ S3/PD_lab_Assignments/ASSG6_B220057CS_PRAVAL/ASSG6_B220057CS_PRAVAL_2_ACTUAL_ANSWER.c
@@ -1,101 +1,40 @@
 #include<stdio.h>
-#include<stdlib.h>
 
-struct stack {
-    int top;
-    int arr[1000];
-};
-
-int stack_empty(struct stack *s) 
-{
-    if (s->top == -1) 
-    {
-        return -1;
-    }
-    else 
-    {
-        return 1;
-    }
-}
-
-int stack_full(struct stack *s, int n) 
-{
-    if (s->top == n-1) 
-    {
-        return -1; 
-    }
-    else 
-    {
-        return 1;
-    }
-}
-
-int push (struct stack *s, int n, int x)
-{
-    s->top++;
-    s->arr[s->top] = x;
-    return 0;
-}
-
-int pop (struct stack *s)
-{
-    int x = s->arr[s->top];
-    s->top--;
-    return x;
-}
-
-int peek(struct stack *s)
-{
-    if(stack_empty(s) == -1)
-    {
-        return -1;
-    }
-    else
-    {
-        return s->arr[s->top];
-    }
-}
+//this is supposed to be a stack implementation but couldn't make it work
 
 int main()
 {
-    int size;
-    scanf("%d",  &size);
-    int arr[1000];
-    
-    struct stack *s = malloc(sizeof(struct stack));
-    s->top = -1;
-    int *next_greater = malloc(size);
-    int counter = 0;
-    
-    int current, next;
+    int size, next;
+    scanf("%d", &size);
+    int arr[size], newarr[size];
     for(int i = 0; i < size; i++)
     {
         scanf("%d", &arr[i]);
     }
-
     for(int i = 0; i < size; i++)
     {
-        while(stack_empty(s) != -1 && arr[peek(s)] < arr[i])
+        next = -1;
+        for(int j = i + 1; j < size; j++)
         {
-            int x = pop(s);
-            next_greater[x] = arr[i];
-            //printf("%d --> %d\n", x, arr[i]);
+            if (arr[i] < arr[j])
+            {
+                next = arr[j];
+                break;
+            }
         }
-        
-        push(s, size, i);
+        newarr[i] = next;
     }
-    
-    while(stack_empty(s) != -1)
-    {
-        int x = pop(s);
-        next_greater[x] = -1;
-    }    
 
     for(int i = 0; i < size; i++)
     {
-        printf("%d", next_greater[i]);
-        if(i != size-1)
-            printf(" ");
+        if(i == 0)
+        {
+            printf("%d", newarr[i]);
+        }
+        else
+        {
+            printf(" %d", newarr[i]);
+        }
     } 
     return 1;
 }
